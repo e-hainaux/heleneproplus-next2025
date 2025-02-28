00147459aa7692066fbdc46df856ec7ea0f972f8
Emilien Hainaux <emilienhainaux@yahoo.fr>
2025-02-28 15:43:21 +0100

    update Cats.js

---
+++ src/components/services-components/Cats.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import styles from "../styles/Modal.module.css";
+import PetsMenu from "../../components/PetsMenu";
+
+import cat from "../public/Images/cat.png";
+
+function Cats() {
+  return (
+    <div className={styles.mainContainer}>
+      <PetsMenu />
+
+      <div className={styles.content}>
+        <div className={styles.animalSection}>
+          <div className={styles.animalImg}>
+            <Image className={styles.objectFit} src={cat} />
+          </div>
+
+          <div className={styles.txtContainer}>
+            <h1>CHATS</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+              ac ligula eget mi ullamcorper viverra. Duis sagittis lacus eget
+              eros ultricies, ac ultricies nisi tincidunt. Nunc accumsan, risus
+              sed aliquam placerat, sapien arcu sodales tellus, eu tincidunt
+              nisi nisl nec lectus. Nam eget eros eu dui tincidunt tristique.
+              Sed euismod, nisi eget feugiat pretium, tellus lectus tincidunt
+              risus, in pulvinar enim eros eget quam. Fusce aliquam neque sit
+              amet felis lacinia, ac ultricies nisi tristique.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cats;
